2c0654c5fdb2f1e80ea0e66b0fe0857b670c0240
anuragrawat <ar4482c@gre.ac.uk>
2023-11-16 10:17:58 +0000

    add DotProductRecursive

--- scalaex/src/DotProductRecursive.scala
+++ scalaex/src/DotProductRecursive.scala
@@ -1,14 +1,10 @@
 import scala.io.StdIn
 
 def dot_product_recursive(value: Vector[Int], value1: Vector[Int]): Int = {
-  // base case
-  if (value.length == 1 & value1.length == 1)
-    return value.head * value1.head
-
-  // recursive case
-  (value.head * value1.head) + dot_product_recursive(value.tail, value1.tail)
+  value.length match
+    case 0=> 0
+    case _ => (value.head * value1.head) + dot_product_recursive(value.tail, value1.tail)
 }
-
 
 object DotProductRecursive extends App {
   var fst_line = StdIn.readLine()
